779b72a81804b1d2dec69a4d36d15029f9e9bccf
lucioomendess-collab <lucioomendess@gmail.com>
2026-09-24 09:03:13 -0300

    primeiro commit

--- MANIPULAÇÃO DE TEXTOS.py
+++ MANIPULAÇÃO DE TEXTOS.py
@@ -1,5 +1,7 @@
 # Escreva um programa que conta o número de vogais e consoantes em uma string
 frase1 = 'Contando o número de vogais e consoantes.'
+numero_de_palavras =    len(frase1.split())
+print(f'A frase tem {numero_de_palavras} palavras.')
 
 
 # Escreva um programa que verifica se uma string é um palíndromo.
@@ -16,11 +18,11 @@ quantidade = len(palavras)
 print(f'A frase contém {quantidade} palavras.')
 
 # Escreva um programa que inverte a ordem das palavras em uma frase.
+frase2 = 'Invertendo a ordem das palavras'
+frase_invertida = " ".join(frase2.split()[::-1])
+print(frase_invertida)
 
 # Escreva um programa que capitalize a primeira letra de cada palavra em uma frase.
 titulo = "aprendendo python no senac"
 titulo_formatado = titulo.title()
 print(titulo_formatado)
-
-
-
