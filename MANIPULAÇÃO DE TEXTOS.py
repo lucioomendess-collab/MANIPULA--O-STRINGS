438c67d55ed8fa78a2cb6c53f4faa65ba839ed9b
lucioomendess-collab <lucioomendess@gmail.com>
2026-09-25 08:34:35 -0300

    Nova alteração

--- MANIPULAÇÃO DE TEXTOS.py
+++ MANIPULAÇÃO DE TEXTOS.py
@@ -24,7 +24,7 @@ else:
     print(f'{palavra} não é um palíndromo')
 
 # Escreva um programa que conta o número de palavras em uma frase.
-frase = input('Digite uma frase')
+frase = input('Digite uma frase: ')
 palavras = frase.split()
 quantidade = len(palavras)
 print(f'A frase contém {quantidade} palavras.')
